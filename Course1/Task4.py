05312282c804452e118ad9cfbfbfae671fad7b88
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-06-06 15:24:08 -0700

    Course1 - finished primary functionality, need to add sorting of the result

--- Course1/Task4.py
+++ Course1/Task4.py
@@ -122,17 +122,83 @@ def test_get_type_of_number():
 
 
 def test_check_calls_data():
-    test_calls_list = list()
+    print("---------------------------------------------")
+    print("->test_check_calls_data:start")
+    test_calls_list = [["78130 00821", "90365 06212", "1/9/2016  6:46:56 AM", "165"],
+                       ["1401111111", "(034)78655", "1/9/2016  7:31", "15"],  # 1401111111 -tele
+                       ["(080)69245029", "90365 06212", "1/9/2016  7:31", "15"],
+                       ["1409999999", "90365 06212", "1/9/2016  7:31", "15"],
+                       ["1408888888", "90365 06212", "1/9/2016  7:31", "15"],  # 1408888888 - telemarketer
+                       ["90365 06212", "1409999999", "1/9/2016  7:31", "15"],  # 1409999999 - not telemarketer
+                       ["1402222222", "83019 53227", "1/9/2016  7:31", "15"],  # 1402222222 - tele
+                       ["1403333333", "1404444444", "1/9/2016  7:31", "15"],  # 1403333333 - tele, 1404444444 - not
+                       ["(04456)69245029", "1405555555", "1/9/2016  7:31", "15"]]  # 1405555555 - not telemarketer
+    check_calls_data(test_calls_list)
+    print("possible_telemarketers_set=" + str(possible_telemarketers_set))
+    assert len(possible_telemarketers_set) == 4
+    assert ("1401111111" in possible_telemarketers_set)
+    assert ("1408888888" in possible_telemarketers_set)
+    assert ("1402222222" in possible_telemarketers_set)
+    assert ("1403333333" in possible_telemarketers_set)
+    assert ("1404444444" not in possible_telemarketers_set)
+    print("->test_check_calls_data: is finished")
+
+
+def test_check_check_texts_data():
+    print("---------------------------------------------")
+    print("->test_check_check_texts_data:start")
+    test_texts_list = [["1401111111", "90365 06212", "1/9/2016  6:03:22 AM"],  # 1401111111 - not telemarketer
+                       ["(04456)69245029", "1403333333", "1/9/2016  7:31"],  # 1403333333 - not telemarketer
+                       ["78130 00821", "(034)78655", "1/9/2016  7:31"]]
+    check_texts_data(test_texts_list)
+    print("possible_telemarketers_set=" + str(possible_telemarketers_set))
+    assert len(possible_telemarketers_set) == 2
+    assert ("1401111111" not in possible_telemarketers_set)  # should be deleted
+    assert ("1408888888" in possible_telemarketers_set)
+    assert ("1402222222" in possible_telemarketers_set)
+    assert ("1403333333" not in possible_telemarketers_set)  # should be deleted
+    assert ("1404444444" not in possible_telemarketers_set)
+    print("->test_check_check_texts_data: is finished")
+
+
+def test_verify_tel_number():
+    print("---------------------------------------------")
+    print("->test_verify_tel_number:start")
+    test_calls_list = [["78130 00821", "90365 06212", "1/9/2016  6:46:56 AM", "165"],
+                       ["1401111111", "(034)78655", "1/9/2016  7:31", "15"],  # 1401111111 -tele
+                       ["(080)69245029", "90365 06212", "1/9/2016  7:31", "15"],
+                       ["1409999999", "90365 06212", "1/9/2016  7:31", "15"],
+                       ["1408888888", "90365 06212", "1/9/2016  7:31", "15"],  # 1408888888 - telemarketer
+                       ["90365 06212", "1409999999", "1/9/2016  7:31", "15"],  # 1409999999 - not telemarketer
+                       ["1402222222", "83019 53227", "1/9/2016  7:31", "15"],  # 1402222222 - tele
+                       ["1403333333", "1404444444", "1/9/2016  7:31", "15"],  # 1403333333 - tele, 1404444444 - not
+                       ["(04456)69245029", "1405555555", "1/9/2016  7:31", "15"]]  # 1405555555 - not telemarketer
+    check_calls_data(test_calls_list)
+    print("possible_telemarketers_set=" + str(possible_telemarketers_set))
+    # case1 - 1408888888 has telemarketer type and exists in possible_telemarketers_set
+    verify_tel_number("1408888888")
+    assert ("1408888888" not in possible_telemarketers_set)  # must be deleted
+    assert len(possible_telemarketers_set) == 3
+    print("case1: possible_telemarketers_set=" + str(possible_telemarketers_set))
+    # case2 - 140777777 has telemarketer type but does not exist in possible_telemarketers_set
+    verify_tel_number("140777777")  # must not change possible_telemarketers_set
+    assert len(possible_telemarketers_set) == 3
+    # case3 - not a telemarketer format and does not exist in possible_telemarketers_set
+    verify_tel_number("84577 7777")  # must not change possible_telemarketers_set
+    assert len(possible_telemarketers_set) == 3
+    print("->test_check_check_texts_data: is finished")
 
 
 def test():
     print("START ALL TESTS....")
     test_get_type_of_number()
     test_check_calls_data()
+    test_check_check_texts_data()
+    test_verify_tel_number()
     print("ALL TESTS FINISHED....")
 
 
 # ----------------------------------------------------------
 
-test()
-# main()
+#test()
+main()
