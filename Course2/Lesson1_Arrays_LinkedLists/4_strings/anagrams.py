905570ab15b83389542145648ffd3fcc5837a32c
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-06-11 18:01:28 -0700

    Course2 - added solution

--- Course2/Lesson1_Arrays_LinkedLists/4_strings/anagrams.py
+++ Course2/Lesson1_Arrays_LinkedLists/4_strings/anagrams.py
@@ -1,4 +1,4 @@
-#TASK
+# TASK
 '''
 The goal of this exercise is to write some code to determine if two strings are anagrams of each other.
 
@@ -36,14 +36,23 @@ def anagram_checker(str1, str2):
     """
 
     # TODO: Write your solution here
+    # Clean strings
+    clean_str_1 = str1.replace(" ", "").lower()
+    clean_str_2 = str2.replace(" ", "").lower()
 
+    if len(clean_str_1) == len(clean_str_2):
+       if sorted(clean_str_1) == sorted(clean_str_2):
+            return True
+
+    return False
 
     pass
 
-# Test Cases
 
-print ("Pass" if not (anagram_checker('water','waiter')) else "Fail")
-print ("Pass" if anagram_checker('Dormitory','Dirty room') else "Fail")
-print ("Pass" if anagram_checker('Slot machines', 'Cash lost in me') else "Fail")
-print ("Pass" if not (anagram_checker('A gentleman','Elegant men')) else "Fail")
-print ("Pass" if anagram_checker('Time and tide wait for no man','Notified madman into water') else "Fail")
+# Test Cases
+print("Pass" if (anagram_checker('rat', 'art')) else "Fail")
+print("Pass" if not (anagram_checker('water', 'waiter')) else "Fail")
+print("Pass" if anagram_checker('Dormitory', 'Dirty room') else "Fail")
+print("Pass" if anagram_checker('Slot machines', 'Cash lost in me') else "Fail")
+print("Pass" if not (anagram_checker('A gentleman', 'Elegant men')) else "Fail")
+print("Pass" if anagram_checker('Time and tide wait for no man', 'Notified madman into water') else "Fail")
