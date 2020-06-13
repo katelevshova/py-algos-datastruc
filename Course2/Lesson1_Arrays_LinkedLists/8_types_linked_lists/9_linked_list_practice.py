5c5af9d1e05a04517f3924e6aa14ed554839333a
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-06-12 21:40:06 -0700

    Course2 - linked list practise

---
+++ Course2/Lesson1_Arrays_LinkedLists/8_types_linked_lists/9_linked_list_practice.py
@@ -0,0 +1,158 @@
+# TASK
+'''
+Linked List Practice
+
+Implement a linked list class. You have to define a few functions that perform
+the desirbale action. Your LinkedList class should be able to:
+
+    Append data to the tail of the list and prepend to the head
+    Search the linked list for a value and return the node
+    Remove a node
+    Pop, which means to return the first node's value and delete the node from the list
+    Insert data at some position in the list
+    Return the size (length) of the linked list
+'''
+
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def to_list(self):
+        out_list = []
+        node = self.head
+        while node:
+            out_list.append(node.value)
+            node = node.next
+        return out_list
+
+    def prepend(self, value):
+        """ Prepend a node to the beginning of the list """
+        if self.head is None:
+            self.head = Node(value)
+            return
+        new_head = Node(value)
+        new_head.next = self.head
+        self.head = new_head
+
+    def append(self, value):
+        """ Append a node to the end of the list """
+        if self.head is None:
+            self.head = Node(value)
+            return
+        node = self.head
+        while node.next:
+            node = node.next
+        node.next = Node(value)
+
+    def search(self, value):
+
+        """ Search the linked list for a node with the requested value and return the node. """
+        if self.head is None:
+            return
+        node = self.head
+        while node.next:
+            if node.value == value:
+                return node
+            node = node.next
+        raise ValueError("Value not found in the list!")
+
+    def remove(self, value):
+        """ Delete the first node with the desired data. """
+        if self.head is None:
+            return
+        node = self.head
+
+        if node.value == value:
+            self.head = self.head.next
+            return
+
+        while node.next:
+            if node.next.value == value:
+                node.next = node.next.next
+                return
+            node = node.next
+
+        raise ValueError("Value is not found in the list!")
+
+    def pop(self):
+        """ Return the first node's value and remove it from the list. """
+        if self.head is None:
+            return None
+        node = self.head
+        self.head = self.head.next
+        return node.value
+
+
+def test():
+    test_prepend()
+    test_append()
+    test_search()
+    test_remove()
+    test_pop()
+
+
+def test_prepend():
+    linked_list = LinkedList()
+    linked_list.prepend(1)
+    assert linked_list.to_list() == [1], "list contents: {}".format(linked_list.to_list())
+
+
+def test_append():
+    # Test append - 1
+    linked_list = LinkedList()
+    linked_list.append(3)
+    linked_list.prepend(2)
+    assert linked_list.to_list() == [2, 3], "list contents: {}".format(linked_list.to_list())
+    # Test append - 2
+    linked_list = LinkedList()
+    linked_list.append(1)
+    assert linked_list.to_list() == [1], "list contents: {}".format(linked_list.to_list())
+    linked_list.append(3)
+    assert linked_list.to_list() == [1, 3], "list contents: {}".format(linked_list.to_list())
+
+
+def test_search():
+    # Test search
+    linked_list = LinkedList()
+    linked_list.prepend(2)
+    linked_list.prepend(1)
+    linked_list.append(4)
+    linked_list.append(3)
+    assert linked_list.search(1).value == 1, "list contents: {}".format(linked_list.to_list())
+    assert linked_list.search(4).value == 4, "list contents: {}".format(linked_list.to_list())
+
+
+def test_remove():
+    linked_list = LinkedList()
+    linked_list.prepend(2)
+    linked_list.prepend(1)
+    linked_list.append(4)
+    linked_list.append(3)
+
+    linked_list.remove(1)
+    assert linked_list.to_list() == [2, 4, 3], "list contents: {}".format(linked_list.to_list())
+    linked_list.remove(3)
+    assert linked_list.to_list() == [2, 4], "list contents: {}".format(linked_list.to_list())
+    linked_list.remove(2)
+    assert linked_list.to_list() == [4], "list contents: {}".format(linked_list.to_list())
+
+
+def test_pop():
+    linked_list = LinkedList()
+    linked_list.append(2)
+    linked_list.append(1)
+    linked_list.append(4)
+    linked_list.append(3)
+    value = linked_list.pop()
+    assert value == 2, "list contents: {}".format(linked_list.to_list())
+    assert linked_list.head.value == 1, "list contents: {}".format(linked_list.to_list())
+
+
+test()
