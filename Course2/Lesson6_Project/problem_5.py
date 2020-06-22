017f06ffb29d30964ae535b5e927df5ec8fbe2dd
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-06-21 18:10:25 -0700

    Course2 - problem5 - added methods for Blockchain class

--- Course2/Lesson6_Project/problem_5.py
+++ Course2/Lesson6_Project/problem_5.py
@@ -11,23 +11,95 @@ and text strings as the data.
 Use your knowledge of linked lists and hashing to create a blockchain implementation.
 
 """
+"""
+NOTE:
+A blockchain is essentially a linked list that contains ordered data, 
+with a few constraints such as:
+
+    Blocks can’t be modified once added; in other words, it is append only.
+    There are specific rules for appending data to it.
+    Its architecture is distributed.
+
+"""
+
 import hashlib
-
-
-def calc_hash(self):
-    sha = hashlib.sha256()
-
-    hash_str = "We are going to encode this string of data!".encode('utf-8')
-
-    sha.update(hash_str)
-
-    return sha.hexdigest()
+import time
 
 
 class Block:
 
-    def __init__(self, timestamp, data, previous_hash):
-        self.timestamp = timestamp
-        self.data = data
-        self.previous_hash = previous_hash
+    def __init__(self, _index, _timestamp, _data, _previous_hash, _nonce=0):
+        self.index = _index
+        self.timestamp = _timestamp
+        self.data = _data
+        self.previous_hash = _previous_hash
         self.hash = self.calc_hash()
+        self.nonce = _nonce
+
+    def calc_hash(self):
+        sha = hashlib.sha256()
+
+        hash_str = "We are going to encode this string of data!".encode('utf-8')
+
+        sha.update(hash_str)
+
+        return sha.hexdigest()
+
+
+class Blockchain:
+    difficulty = 2
+
+    def __init__(self):
+        self.chain = list
+        self.unconfirmed_transactions = []  # store the data of each transaction
+        self.create_genesis_block()
+
+    def create_genesis_block(self):
+        """
+        Is used to initialize the blockchain.
+        Creates an initial block with an index of 0 and a previous hash of 0.
+        """
+        genesis_block = Block(0, time.time(), "Some information", "0")
+        self.chain.append(genesis_block)
+
+    def get_last_block(self):
+        return self.chain[-1]
+
+    def proof_of_work(self, block: Block):
+        computed_hash = block.calc_hash()
+        while not computed_hash.startswith('0' * Blockchain.difficulty):
+            block.nonce += 1
+            computed_hash = block.calc_hash()
+        return computed_hash
+
+    def add_block(self, block: Block, proof):
+        prev_hash = self.get_last_block().hash
+        if prev_hash != block.previous_hash:
+            return False
+        if not self.is_valid_proof(block, proof):
+            return False
+        block.hash = proof
+        self.chain.append(block)
+        return True
+
+    def is_valid_proof(self, block, block_hash):
+        return block_hash.startswith('0' * Blockchain.difficulty) and block_hash == block.calc_hash()
+
+    def add_new_transaction(self, transaction):
+        self.unconfirmed_transactions.append(transaction)
+
+    def mine(self):
+        if not self.unconfirmed_transactions:
+            return False
+
+        last_block = self.get_last_block()
+        new_block = Block(last_block.index + 1, self.unconfirmed_transactions, time.time(), last_block.hash)
+
+        proof = self.proof_of_work(new_block)
+        self.add_block(new_block, proof)
+        self.unconfirmed_transactions = []
+        return new_block.index
+
+
+def create_blockchain():
+    print("->create_blockchain:")
