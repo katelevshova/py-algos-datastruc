612ad29b50e702e5ba54c6307090d59e11343710
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-06-23 16:51:13 -0700

    Course2 - problem6 - fixed printing linked list, added ListOperations

--- Course2/Lesson6_Project/problem_6.py
+++ Course2/Lesson6_Project/problem_6.py
@@ -20,7 +20,7 @@ class Node:
         self.next = None
 
     def __repr__(self):
-        return str(self.value)
+        return "Node: "+str(self.value)
 
 
 class LinkedList:
@@ -31,7 +31,11 @@ class LinkedList:
         cur_head = self.head
         out_string = ""
         while cur_head:
-            out_string += str(cur_head.value) + " -> "
+            out_string += str(cur_head.value)
+
+            if cur_head.next is not None:
+                out_string += " -> "
+
             cur_head = cur_head.next
         return out_string
 
@@ -55,3 +59,63 @@ class LinkedList:
             node = node.next
 
         return size
+
+class ListOperations:
+    def union(self, linked_list_1, linked_list_2):
+        print("->union:")
+        print("linked_list_1= "+str(linked_list_1))
+        print("linked_list_2= " + str(linked_list_2))
+
+        result_list = LinkedList()
+
+        if linked_list_1.size() < linked_list_2:
+            current_head = linked_list_2.head
+            while current_head
+
+        return result_list
+
+    def intersection(self, linked_list_1, linked_list_2):
+        print("->intersection:")
+
+
+# TEST CASES: start----------------------------------------------
+def test_union_intersection_1():
+    print("->test_union_intersection_1: start")
+
+    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
+    element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
+
+    linked_list_1 = LinkedList()
+    linked_list_2 = LinkedList()
+
+    for i in element_1:
+        linked_list_1.append(i)
+
+    for i in element_2:
+        linked_list_2.append(i)
+
+    listOperations = ListOperations()
+
+    assert linked_list_1.size() == len(element_1)
+    assert linked_list_2.size() == len(element_2)
+
+    # all uniqie from 2, all unique from 1, both
+    expected_union = list(set(element_1).union(set(element_2)))  #[32, 65, 2, 35, 3, 4, 6, 1, 9, 11, 21]
+    print("expected_union="+str(expected_union))
+
+    result_union = listOperations.union(linked_list_1, linked_list_2)
+
+    # assert expected_union == result_union, "expected_union is {}, actual result = {}".format(expected_union,
+      #                                                                                       result_union)
+    print("result_union= "+str(result_union))
+
+    print("->test_union_intersection_1: end")
+
+
+# TEST CASES: end----------------------------------------------
+
+def test():
+    test_union_intersection_1()
+
+
+test()
