bf49362ab7e49f06503290ccc05b50e1e9dbecdc
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-06-24 11:53:04 -0700

    Course2 - problem6 - added union(), search(), convert_set_to_linked_list()

--- Course2/Lesson6_Project/problem_6.py
+++ Course2/Lesson6_Project/problem_6.py
@@ -20,7 +20,7 @@ class Node:
         self.next = None
 
     def __repr__(self):
-        return "Node: "+str(self.value)
+        return "Node: " + str(self.value)
 
 
 class LinkedList:
@@ -60,27 +60,62 @@ class LinkedList:
 
         return size
 
+
 class ListOperations:
-    def union(self, linked_list_1, linked_list_2):
-        print("->union:")
-        print("linked_list_1= "+str(linked_list_1))
-        print("linked_list_2= " + str(linked_list_2))
 
+    def union(self, linked_list_1, linked_list_2) -> LinkedList:
+        # print("->union:")
+        # print("linked_list_1= " + str(linked_list_1))
+        # print("linked_list_2= " + str(linked_list_2))
+
+        union_set = set()
+
+        node1 = linked_list_1.head
+        node2 = linked_list_2.head
+
+        # print("node1= " + str(node1) + ", value=" + str(node1.value))
+        # print("node2= " + str(node2) + ", value=" + str(node2.value))
+
+        while node1 != None or node2 != None:
+            if node1 != None:
+                union_set.add(node1.value)
+                node1 = node1.next
+            if node2 != None:
+                union_set.add(node2.value)
+                node2 = node2.next
+
+        # print("union_set= " + str(union_set))
+        return self.convert_set_to_linked_list(union_set)
+
+    def convert_set_to_linked_list(self, _set) -> LinkedList:
         result_list = LinkedList()
-
-        if linked_list_1.size() < linked_list_2:
-            current_head = linked_list_2.head
-            while current_head
-
+        for item in _set:
+            result_list.append(item)
         return result_list
+
+    # This Function checks whether the value
+    # x present in the linked list
+    def search(self, x, linked_list):
+
+        # Initialize current to head
+        current = linked_list.head
+
+        # loop till current not equal to None
+        while current != None:
+            if current.value == x:
+                return True  # data found
+
+            current = current.next
+
+        return False  # Data Not found
 
     def intersection(self, linked_list_1, linked_list_2):
         print("->intersection:")
 
 
 # TEST CASES: start----------------------------------------------
-def test_union_intersection_1():
-    print("->test_union_intersection_1: start")
+def test_union_1():
+    print("->test_union_1: start")
 
     element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
     element_2 = [6, 32, 4, 9, 6, 1, 11, 21, 1]
@@ -99,23 +134,29 @@ def test_union_intersection_1():
     assert linked_list_1.size() == len(element_1)
     assert linked_list_2.size() == len(element_2)
 
-    # all uniqie from 2, all unique from 1, both
-    expected_union = list(set(element_1).union(set(element_2)))  #[32, 65, 2, 35, 3, 4, 6, 1, 9, 11, 21]
-    print("expected_union="+str(expected_union))
-
+    # all uniqie from ll2, all unique from ll1, both unique
+    expected_union = listOperations.convert_set_to_linked_list(
+        set(element_1).union(set(element_2)))  # [32, 65, 2, 35, 3, 4, 6, 1, 9, 11, 21]
     result_union = listOperations.union(linked_list_1, linked_list_2)
+    print("expected_union= " + str(expected_union))
+    print("result_union= " + str(result_union))
 
-    # assert expected_union == result_union, "expected_union is {}, actual result = {}".format(expected_union,
-      #                                                                                       result_union)
-    print("result_union= "+str(result_union))
+    assert expected_union.size() == result_union.size()
+    node = expected_union.head
+    while node != None:
+        assert listOperations.search(node.value, result_union)
+        node = node.next
 
-    print("->test_union_intersection_1: end")
+    print("->test_union_1: end")
 
+def test_union_2():
+    print("->test_union_2: start")
+    print("->test_union_2: end")
 
 # TEST CASES: end----------------------------------------------
 
 def test():
-    test_union_intersection_1()
+    test_union_1()
 
 
 test()
