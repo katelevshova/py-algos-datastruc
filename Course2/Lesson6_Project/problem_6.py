5defabee4072c51660b0af920a63c7bea89c8c5c
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-06-24 14:20:35 -0700

    Course2 - problem6 - renamed listOperations to list_operations, finished test_intersection_2

--- Course2/Lesson6_Project/problem_6.py
+++ Course2/Lesson6_Project/problem_6.py
@@ -129,9 +129,9 @@ def test_convert_set_to_linked_list():
     print("=============================================================================")
     print("->test_convert_set_to_linked_list: start")
 
-    listOperations = ListOperations()
+    list_operations = ListOperations()
     test_set = set({1, 7, 5, 4})
-    result_linked_list = listOperations.convert_set_to_linked_list(test_set)
+    result_linked_list = list_operations.convert_set_to_linked_list(test_set)
     # print("result_linked_list= "+str(result_linked_list))
 
     node = result_linked_list.head
@@ -145,16 +145,16 @@ def test_convert_set_to_linked_list():
 def test_search():
     print("=============================================================================")
     print("->test_search: start")
-    listOperations = ListOperations()
+    list_operations = ListOperations()
     element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 21]
     linked_list_1 = LinkedList()
     for i in element_1:
         linked_list_1.append(i)
 
-    assert listOperations.search(4, linked_list_1)
-    assert listOperations.search(6, linked_list_1)
-    assert listOperations.search(999, linked_list_1) == False
-    assert listOperations.search(None, linked_list_1) == False
+    assert list_operations.search(4, linked_list_1)
+    assert list_operations.search(6, linked_list_1)
+    assert list_operations.search(999, linked_list_1) == False
+    assert list_operations.search(None, linked_list_1) == False
     print("->test_search: end")
 
 
@@ -175,22 +175,22 @@ def test_union_1():
     for i in element_2:
         linked_list_2.append(i)
 
-    listOperations = ListOperations()
+    list_operations = ListOperations()
 
     assert linked_list_1.size() == len(element_1)
     assert linked_list_2.size() == len(element_2)
 
     # all uniqie from ll2, all unique from ll1, both unique
-    expected_union_linked_list = listOperations.convert_set_to_linked_list(
+    expected_union_linked_list = list_operations.convert_set_to_linked_list(
         set(element_1).union(set(element_2)))  # [32, 65, 2, 35, 3, 4, 6, 1, 9, 11, 21]
-    result_union_linked_list = listOperations.union(linked_list_1, linked_list_2)
+    result_union_linked_list = list_operations.union(linked_list_1, linked_list_2)
     print("expected_union_linked_list= " + str(expected_union_linked_list))
     print("result_union_linked_list= " + str(result_union_linked_list))
 
     assert expected_union_linked_list.size() == result_union_linked_list.size()
     node = expected_union_linked_list.head
     while node != None:
-        assert listOperations.search(node.value, result_union_linked_list)
+        assert list_operations.search(node.value, result_union_linked_list)
         node = node.next
 
     print("->test_union_1: end")
@@ -213,22 +213,22 @@ def test_union_2():
     for i in element_2:
         linked_list_2.append(i)
 
-    listOperations = ListOperations()
+    list_operations = ListOperations()
 
     assert linked_list_1.size() == len(element_1)
     assert linked_list_2.size() == len(element_2)
 
     # all uniqie from ll2, all unique from ll1, both unique
-    expected_union_linked_list = listOperations.convert_set_to_linked_list(
+    expected_union_linked_list = list_operations.convert_set_to_linked_list(
         set(element_1).union(set(element_2)))  # [32, 65, 2, 35, 3, 4, 6, 1, 9, 11, 21]
-    result_union_linked_list = listOperations.union(linked_list_1, linked_list_2)
+    result_union_linked_list = list_operations.union(linked_list_1, linked_list_2)
     print("expected_union_linked_list= " + str(expected_union_linked_list))
     print("result_union_linked_list= " + str(result_union_linked_list))
 
     assert expected_union_linked_list.size() == result_union_linked_list.size()
     node = expected_union_linked_list.head
     while node != None:
-        assert listOperations.search(node.value, result_union_linked_list)
+        assert list_operations.search(node.value, result_union_linked_list)
         node = node.next
 
     print("->test_union_2: end")
@@ -251,22 +251,22 @@ def test_union_3():
     for i in element_2:
         linked_list_2.append(i)
 
-    listOperations = ListOperations()
+    list_operations = ListOperations()
 
     assert linked_list_1.size() == len(element_1)
     assert linked_list_2.size() == len(element_2)
 
     # all uniqie from ll2, all unique from ll1, both unique
-    expected_union_linked_list = listOperations.convert_set_to_linked_list(
+    expected_union_linked_list = list_operations.convert_set_to_linked_list(
         set(element_1).union(set(element_2)))  # [32, 65, 2, 35, 3, 4, 6, 1, 9, 11, 21]
-    result_union_linked_list = listOperations.union(linked_list_1, linked_list_2)
+    result_union_linked_list = list_operations.union(linked_list_1, linked_list_2)
     print("expected_union_linked_list= " + str(expected_union_linked_list))
     print("result_union_linked_list= " + str(result_union_linked_list))
 
     assert expected_union_linked_list.size() == result_union_linked_list.size()
     node = expected_union_linked_list.head
     while node != None:
-        assert listOperations.search(node.value, result_union_linked_list)
+        assert list_operations.search(node.value, result_union_linked_list)
         node = node.next
 
     print("->test_union_3: end")
@@ -288,15 +288,15 @@ def test_intersection_1():
     for i in element_2:
         linked_list_2.append(i)
 
-    listOperations = ListOperations()
+    list_operations = ListOperations()
 
     assert linked_list_1.size() == len(element_1)
     assert linked_list_2.size() == len(element_2)
 
     # expected: 4, 21, 6
     expected_intersection_set = set([value for value in element_1 if value in element_2])
-    expected_intersection_linked_list = listOperations.convert_set_to_linked_list(expected_intersection_set)
-    result_intersection_linked_list = listOperations.intersection(linked_list_1, linked_list_2)
+    expected_intersection_linked_list = list_operations.convert_set_to_linked_list(expected_intersection_set)
+    result_intersection_linked_list = list_operations.intersection(linked_list_1, linked_list_2)
     print("expected_intersection_linked_list= " + str(expected_intersection_linked_list))
     print("result_intersection_linked_list= " + str(result_intersection_linked_list))
 
@@ -309,6 +309,37 @@ def test_intersection_1():
     print("->test_intersection_1: end")
 
 
+def test_intersection_2():
+    print("=============================================================================")
+    print("->test_intersection_2: start")
+    linked_list_1 = LinkedList()
+    linked_list_2 = LinkedList()
+
+    element_1 = [3, 2, 4, 35, 6, 65, 6, 4, 3, 23]
+    element_2 = [1, 7, 8, 9, 11, 21, 1]
+    # expected: []
+
+    for i in element_1:
+        linked_list_1.append(i)
+
+    for i in element_2:
+        linked_list_2.append(i)
+
+    assert linked_list_1.size() == len(element_1)
+    assert linked_list_2.size() == len(element_2)
+
+    list_operations = ListOperations()
+    expected_intersection_linked_list = LinkedList() # expected empty
+    result_intersection_linked_list = list_operations.intersection(linked_list_1, linked_list_2)
+    print("expected_intersection_linked_list= " + str(expected_intersection_linked_list))
+    print("result_intersection_linked_list= " + str(result_intersection_linked_list))
+
+    assert result_intersection_linked_list.size() == expected_intersection_linked_list.size()
+    assert result_intersection_linked_list.size() == 0
+
+    print("->test_intersection_2: end")
+
+
 # TEST CASES: end----------------------------------------------
 
 def test():
@@ -318,6 +349,7 @@ def test():
     test_union_2()
     test_union_3()
     test_intersection_1()
+    test_intersection_2()
 
 
 test()
