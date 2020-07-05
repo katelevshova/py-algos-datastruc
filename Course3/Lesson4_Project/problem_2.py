692aeef9afccc8ed5942401d9e84a025c4dc3632
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-07-05 15:56:25 -0700

    Course3 - problem2

--- Course3/Lesson4_Project/problem_2.py
+++ Course3/Lesson4_Project/problem_2.py
@@ -11,38 +11,105 @@ Input: nums = [4,5,6,7,0,1,2], target = 0, Output: 4
 """
 
 
-def rotated_array_search(input_list, number):
+def rotated_array_search(input_list, target):
     """
     Find the index by searching in a rotated sorted array
 
     Args:
-       input_list(array), number(int): Input array to search and the target
+       input_list(array), target(int): Input array to search and the target
     Returns:
        int: Index or -1
     """
+    return search(input_list, target, 0, len(input_list) - 1)
 
 
-pass
+def search(input_list: list, target: int, left_index: int, right_index: int) -> int:
+    if right_index < left_index:
+        return -1
 
+    middle_index = (left_index + right_index) // 2
+    if target == input_list[middle_index]:
+        return middle_index  # Found element
 
-def linear_search(input_list, number):
-    for index, element in enumerate(input_list):
-        if element == number:
-            return index
+    # The left or right half must be normally ordered. We need to find out which side and then use the normally
+    # ordered half to figure out which side to search to find target.
+
+    left_value = input_list[left_index]
+    mid_value = input_list[middle_index]
+    right_value = input_list[right_index]
+
+    if left_value < mid_value:  # left is normally ordered
+        if left_value <= target < mid_value:
+            return search(input_list, target, left_index, middle_index - 1)  # search LEFT
+        else:
+            return search(input_list, target, middle_index + 1, right_index)  # search RIGHT
+    elif left_value > mid_value:  # right is normally ordered
+        if mid_value < target <= right_value:
+            return search(input_list, target, middle_index + 1, right_index)  # search RIGHT
+        else:
+            return search(input_list, target, left_index, middle_index - 1)  # search LEFT
+    elif left_value == mid_value:  # lef or right half is all repeats
+        if mid_value != right_value:  # if right is different search it
+            return search(input_list, target, middle_index + 1, right_index)  # search RIGHT
+        else:  # search both halves
+            result = search(input_list, target, left_index, middle_index - 1)  # search LEFT
+            if result == -1:
+                return search(input_list, target, middle_index + 1, right_index)  # search RIGHT
+            else:
+                return result
     return -1
 
 
-def test_function(test_case):
-    input_list = test_case[0]
-    number = test_case[1]
-    if linear_search(input_list, number) == rotated_array_search(input_list, number):
-        print("Pass")
-    else:
-        print("Fail")
+def test_rotated_array_search_1():
+    print("----------------------------------------")
+    print("->test_rotated_array_search_1: start")
+    result_actual = rotated_array_search([6, 7, 8, 9, 10, 1, 2, 3, 4], 6)
+    result_expected = 0
+    assert result_actual == result_expected, "result_expected = {}, result_actual = {}".format(result_expected,
+                                                                                               result_actual)
+    print("->test_rotated_array_search_1: end")
 
 
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 6])
-test_function([[6, 7, 8, 9, 10, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 8])
-test_function([[6, 7, 8, 1, 2, 3, 4], 1])
-test_function([[6, 7, 8, 1, 2, 3, 4], 10])
+def test_rotated_array_search_2():
+    print("----------------------------------------")
+    print("->test_rotated_array_search_2: start")
+    result_actual = rotated_array_search([6, 7, 8, 9, 10, 1, 2, 3, 4], 1)
+    result_expected = 5
+    assert result_actual == result_expected, "result_expected = {}, result_actual = {}".format(result_expected,
+                                                                                               result_actual)
+    print("->test_rotated_array_search_2: end")
+
+
+def test_rotated_array_search_3():
+    print("----------------------------------------")
+    print("->test_rotated_array_search_3: start")
+    result_actual = rotated_array_search([6, 7, 8, 1, 2, 3, 4], 8)
+    result_expected = 2
+    assert result_actual == result_expected, "result_expected = {}, result_actual = {}".format(result_expected,
+                                                                                               result_actual)
+    print("->test_rotated_array_search_3: end")
+
+
+def test_rotated_array_search_4():
+    print("----------------------------------------")
+    print("->test_rotated_array_search_4: start")
+    rotated_array_search([6, 7, 8, 1, 2, 3, 4], 1)
+    print("->test_rotated_array_search_4: end")
+
+
+def test_rotated_array_search_5():
+    print("----------------------------------------")
+    print("->test_rotated_array_search_5: start")
+    rotated_array_search([6, 7, 8, 1, 2, 3, 4], 10)
+    print("->test_rotated_array_search_5: end")
+
+
+def test():
+    test_rotated_array_search_1()
+    test_rotated_array_search_2()
+    test_rotated_array_search_3()
+    # test_rotated_array_search_4()
+    # test_rotated_array_search_5()
+
+
+test()
