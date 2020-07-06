bd1c3fdd38648c084a02db682ef5a8f98d86e664
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-07-06 14:17:12 -0700

    Course3 - problem3 - added test cases

--- Course3/Lesson4_Project/problem_3.py
+++ Course3/Lesson4_Project/problem_3.py
@@ -21,4 +21,54 @@ def rearrange_digits(input_list):
     Returns:
        (int),(int): Two maximum sums
     """
-    print("")
+
+    # sort the list
+
+    print("->rearrange_digits: input_list= ")
+
+
+def sort_list(input_list) -> list:
+    return
+
+
+def test():
+    test_sort_list()
+
+
+def test_rearrange_digits_1():
+    print("---------------------------------")
+    print("->test_rearrange_digits_1: start")
+    actual_result = rearrange_digits([4, 6, 2, 5, 9, 8])
+    expected_result = [964, 852]
+    assert expected_result == actual_result, "expected is {}, actual is {}".format(expected_result, actual_result)
+    print("->test_rearrange_digits_1: end")
+
+
+def test_rearrange_digits_2():
+    print("---------------------------------")
+    print("->test_rearrange_digits_2: start")
+    actual_result = rearrange_digits([1, 2, 3, 4, 5])
+    expected_result = [542, 31]
+    assert expected_result == actual_result, "expected is {}, actual is {}".format(expected_result, actual_result)
+    print("->test_rearrange_digits_2: end")
+
+
+def test_rearrange_digits_3():
+    print("---------------------------------")
+    print("->test_rearrange_digits_3: start")
+    actual_result = rearrange_digits([1, 2, 3, 4, 5])
+    expected_result = [531, 42]
+    assert expected_result == actual_result, "expected is {}, actual is {}".format(expected_result, actual_result)
+    print("->test_rearrange_digits_3: end")
+
+
+def test_sort_list():
+    print("---------------------------------")
+    print("->test_sort_list: start")
+    actual_result = sort_list([4, 6, 2, 5, 9, 8])
+    expected_result = [2, 4, 5, 6, 8, 9]
+    assert expected_result == actual_result, "expected is {}, actual is {}".format(expected_result, actual_result)
+    print("->test_sort_list: end")
+
+
+test()
