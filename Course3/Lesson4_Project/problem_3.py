81cd5712b510a63f4f2db0bcf9345103b836742f
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-07-06 16:44:49 -0700

    Course3 - problem3 - added test cases for sorting

--- Course3/Lesson4_Project/problem_3.py
+++ Course3/Lesson4_Project/problem_3.py
@@ -18,13 +18,19 @@ class QuickSort:
             tmp: int = input_list[lower_index]
             input_list[lower_index] = input_list[higher_index]
             input_list[higher_index] = tmp
-        print("->swap_numbers: "+str(input_list))
+        # print("->swap_numbers: "+str(input_list))
 
     def partition(self, input_list, low_index: int, high_index: int):
         index_lower = low_index - 1  # index of the smaller element
         pivot_element = input_list[high_index]
 
+        if pivot_element is None:
+            raise ValueError ("Pivot element is None")
+
         for index in range(low_index, high_index):
+            if input_list[index] is None:
+                raise ValueError("Element with index={} is None".format(index))
+
             if input_list[index] <= pivot_element:
                 index_lower += 1
                 self.swap_numbers(input_list, index_lower, index)
@@ -57,11 +63,14 @@ def sort_list(input_list) -> list:
     print("->sort_list: input_list= "+str(input_list))
     quick_sort: QuickSort = QuickSort()
     quick_sort.perform(input_list, 0, len(input_list)-1)
+    print("->sort_list: result= " + str(input_list))
     return input_list
 
 
 def test():
-    test_sort_list()
+    test_sort_list_1()
+    test_sort_list_2()
+    # test_sort_list_3()
 
 
 def test_rearrange_digits_1():
@@ -91,13 +100,29 @@ def test_rearrange_digits_3():
     print("->test_rearrange_digits_3: end")
 
 
-def test_sort_list():
+def test_sort_list_1():
     print("---------------------------------")
-    print("->test_sort_list: start")
+    print("->test_sort_list_1: start")
     actual_result = sort_list([4, 6, 2, 5, 9, 8])
     expected_result = [2, 4, 5, 6, 8, 9]
     assert expected_result == actual_result, "expected is {}, actual is {}".format(expected_result, actual_result)
-    print("->test_sort_list: end")
+    print("->test_sort_list_1: end")
+
+
+def test_sort_list_2():
+    print("---------------------------------")
+    print("->test_sort_list_2: start")
+    actual_result = sort_list([4, 6, 0, 2, 5, 9, 9, 8])
+    expected_result = [0, 2, 4, 5, 6, 8, 9, 9]
+    assert expected_result == actual_result, "expected is {}, actual is {}".format(expected_result, actual_result)
+    print("->test_sort_list_2: end")
+
+
+def test_sort_list_3():
+    print("---------------------------------")
+    print("->test_sort_list_3: start")
+    sort_list([4, 6, None, 2, 5, 9, None, 8])
+    print("->test_sort_list_3: end")
 
 
 test()
