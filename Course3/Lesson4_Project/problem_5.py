ad9f202fe6b7d7ffa2d6a767b454ed888027e954
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-07-09 15:14:47 -0700

    Course3 - problem5 - changed print for debugging

--- Course3/Lesson4_Project/problem_5.py
+++ Course3/Lesson4_Project/problem_5.py
@@ -7,19 +7,21 @@ TASK5:
 # Represents a single node in the Trie
 class TrieNode:
     def __init__(self):
-       # print("Create new TrieNode")
+        # print("Create new TrieNode")
         self.char_nodes_dict = {}
         self.last = False
 
     def __str__(self):
-        result = "TrieNode :  "
-        for child in self.char_nodes_dict:
-            result += child + "->"
-        return result[:-2]
+        result = ""
+        for key, value in self.char_nodes_dict.items():
+            result += str(key) + " : " + str(value)
+        # for child in self.char_nodes_dict:
+        #    result += child + "->"
+        return result
 
     def insert(self, char):
-        # print("[TrieNode]-> insert: char= "+char)
         if not self.char_nodes_dict.get(char):
+            print("1. [TrieNode]-> insert: char= " + char)
             self.char_nodes_dict[char] = TrieNode()
 
 
@@ -28,16 +30,21 @@ class Trie:
     def __init__(self):
         # Initialize this Trie (add a root node)
         self.root = TrieNode()
+        self.words_set = set()
 
     def insert(self, word):
         # Add a word to the Trie
+        print("-----------------------------")
         print("[Trie]->insert: word= " + word)
         node = self.root
+        self.words_set.add(word)
 
         for char_key in list(word):
+            print("char_key= " + char_key)
             node.insert(char_key)
+            print("2. root node= " + str(node))
             node = node.char_nodes_dict[char_key]
-            print("char_key= "+char_key+", "+str(node))
+            print("3. child node= " + str(node))
         node.last = True
 
 
