84cd36e8dd72b666fda583b8835872bebef24600
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-07-10 17:59:01 -0700

    Course3 - problem5 - added test cases and comments

--- Course3/Lesson4_Project/problem_5.py
+++ Course3/Lesson4_Project/problem_5.py
@@ -1,5 +1,18 @@
 """
 TASK5:
+Building a Trie in Python
+
+Before we start let us reiterate the key components of a Trie or Prefix Tree.
+A trie is a tree-like data structure that stores a dynamic set of strings.
+Tries are commonly used to facilitate operations like predictive text or autocomplete features
+on mobile phones or web search.
+
+Before we move into the autocomplete function we need to create a working trie for storing strings.
+We will create two classes:
+    A Trie class that contains the root node (empty string)
+    A TrieNode class that exposes the general functionality of the Trie,
+    like inserting a word or finding the node which represents a prefix.
+
 
 """
 
@@ -22,15 +35,15 @@ class TrieNode:
             self.char_nodes_dict[char] = TrieNode()
 
     '''
-    Recursive function that collects the suffix for 
+    Function that collects the suffix for 
     all complete words below this point.
     For example, if our Trie contains the words ["fun", "function", "factory"] and
     we ask for suffixes from the f node, we would expect 
-    to receive ["un", "unction", "actory"] back from node.suffixes().
+    to receive ["un", "unction", "actory"] back.
     '''
 
     def suffixes(self) -> list:
-        print("[TrieNode]->suffixes: ")
+        # print("[TrieNode]->suffixes: ")
         result_list = []
         tmp_suffix = ""
 
@@ -81,6 +94,7 @@ class Trie:
             # print("char="+char)
             if not node.char_nodes_dict.get(char):
                 found = False
+                print("not found!")
                 break
 
             node = node.char_nodes_dict[char]
@@ -89,20 +103,73 @@ class Trie:
         return node, found
 
 
-MyTrie = Trie()
-wordList = [
-    "ant", "anthology", "antagonist", "antonym",
-    "fun", "function", "factory",
-    "trie", "trigger", "trigonometry", "tripod"
-]
-for word in wordList:
-    MyTrie.insert(word)
+def test_suffixes_1():
+    print("--------------------------------------------")
+    print("->test_suffixes_1: start")
+    trie = Trie()
+    word_list = [
+        "ant", "anthology", "antagonist", "antonym",
+        "fun", "function", "factory",
+        "trie", "trigger", "trigonometry", "tripod"
+    ]
+    for word in word_list:
+        trie.insert(word)
 
-prefix = 'ant'
-prefixNode, isFound = MyTrie.find(prefix)
-print("prefixNode= " + str(prefixNode))
-print("isFound= " + str(isFound))
+    prefixNode, isFound = trie.find('ant')
+    print("prefixNode= " + str(prefixNode))
 
-if isFound:
+    assert isFound
+
     suffixes = prefixNode.suffixes()
     print("suffixes= " + str(suffixes))
+    assert suffixes == ['hology', 'agonist', 'onym']
+    print("->test_suffixes_1: end")
+
+
+def test_suffixes_2():
+    print("--------------------------------------------")
+    print("->test_suffixes_2: start")
+    trie = Trie()
+    word_list = [
+        "ant", "anthology", "antagonist", "antonym",
+        "fun", "function", "factory",
+        "trie", "trigger", "trigonometry", "tripod"
+    ]
+    for word in word_list:
+        trie.insert(word)
+
+    prefixNode, isFound = trie.find('f')
+    print("prefixNode= " + str(prefixNode))
+
+    assert isFound
+
+    suffixes = prefixNode.suffixes()
+    print("suffixes= " + str(suffixes))
+    assert suffixes == ['un', 'unction', 'actory']
+    print("->test_suffixes_2: end")
+
+
+def test_find_3():
+    print("--------------------------------------------")
+    print("->test_find_3: start")
+    trie = Trie()
+    word_list = [
+        "ant", "anthology", "antagonist", "antonym",
+        "fun", "function", "factory",
+        "trie", "trigger", "trigonometry", "tripod"
+    ]
+    for word in word_list:
+        trie.insert(word)
+
+    prefixNode, isFound = trie.find('g')
+    assert isFound == False
+    print("->test_find_3: end")
+
+
+def test():
+    test_suffixes_1()
+    test_suffixes_2()
+    test_find_3()
+
+
+test()
