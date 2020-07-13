0073e7b8442481924bd0be08e9faad764657f6da
Kate Levshova <11220246+katelevshova@users.noreply.github.com>
2020-07-13 14:17:44 -0700

    Course3 - problem7 - added comments and returned types for the methods

--- Course3/Lesson4_Project/problem_7.py
+++ Course3/Lesson4_Project/problem_7.py
@@ -23,10 +23,9 @@ class RouteTrieNode:
         self.handler = None
 
     def insert(self, dir_name):
-        # if not self.dir_nodes_dict.get(dir_name):
         self.dir_nodes_dict[dir_name] = RouteTrieNode()
 
-    def __str__(self):
+    def __str__(self) -> str:
         result = ""
         for key, value in self.dir_nodes_dict.items():
             result += "{" + str(key) + ":{" + str(value) + "}}"
@@ -51,7 +50,12 @@ class RouteTrie:
             node.handler = handler_str
         print("     handler= " + str(node.handler) + "\n")
 
-    def find(self, dir_list):
+    def find(self, dir_list) -> str:
+        """
+        Finds the handler for the path represented as dir_list
+        :param dir_list: - list created based on the lookup path
+        :return: string value of the handler
+        """
         print("->find: dir_list=" + str(dir_list))
         node = self.root
 
@@ -65,7 +69,13 @@ class RouteTrie:
             # print("node=" + str(node))
         return self.get_handler_value(node.handler)
 
-    def get_handler_value(self, handler_str):
+    def get_handler_value(self, handler_str) -> str:
+        """
+        Returns string handler value. If the handler value is None or "", returns "not found handler"
+        :param handler_str: string
+        :return: string
+        """
+
         if handler_str is None or handler_str is "":
             return "not found handler"
         else:
@@ -77,7 +87,14 @@ class Router:
         self.route_trie = RouteTrie()
         self.add_handler(path_str, handler_str)
 
-    def dir_name_checker(self, path_str):
+    def dir_name_checker(self, path_str) -> str:
+        """
+        Converts path to a proper format using "Root/" prefix.
+        Example_1: "", "/", "Root", "Root/" are converted to "Root/"
+        Example_2: "/Home" converts to "Root/Home"
+        :param path_str:
+        :return: str - converted string value
+        """
         if path_str == "" or path_str == "/" or path_str == "Root" or path_str == "Root/":
             return "Root/"
         elif path_str[0] == "/":
@@ -85,8 +102,6 @@ class Router:
         else:
             return "Root/" + path_str
 
-    # Create a new RouteTrie for holding our routes
-    # You could also add a handler for 404 page not found responses as well!
     def add_handler(self, path_str='', handler_str=''):
         print("->add_handler: path_str= " + str(path_str) + ", handler_str= " + str(handler_str))
 
@@ -96,23 +111,19 @@ class Router:
         else:
             print("     path_str must be initialized! currently it's value is None.")
 
-    # Add a handler for a path
-    # You will need to split the path and pass the pass parts
-    # as a list to the RouteTrie
-
-    def lookup(self, path_str):
-        # lookup path (by parts) and return the associated handler
-        # you can return None if it's not found or
-        # return the "not found" handler if you added one
-        # bonus points if a path works with and without a trailing slash
-        # e.g. /about and /about/ both return the /about handler
+    def lookup(self, path_str) -> str:
+        """
+        Searches the path an returns the associated handler
+        :param path_str: lookup path, /about and /about/ both return the /about handler
+        :return handler: string
+        """
         print("->lookup: path_str= " + str(path_str))
         checked_path = self.dir_name_checker(path_str)
         split_path_list = self.split_path(checked_path)
         print("root= " + str(self.route_trie.root))
 
         result_handler = self.route_trie.find(split_path_list)
-        print("->lookup: result_handler= " + str(result_handler) +"\n")
+        print("->lookup: result_handler= " + str(result_handler) + "\n")
         return result_handler
 
     def split_path(self, path_str) -> list:
