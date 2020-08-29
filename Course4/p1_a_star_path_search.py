2dd80aef7719cf8be9bb06cc064f6a74708094e8
Kateryna Levshova <>
2020-08-28 17:41:02 -0700

    Changed Task comment

--- Course4/p1_a_star_path_search.py
+++ Course4/p1_a_star_path_search.py
@@ -3,8 +3,7 @@ import math
 
 """
 TASK:
-Build a route-planning algorithm like the one used in Google Maps to calculate
-the shortest path between two points on a map.
+shortest path between two points on a map
 """
 
 
